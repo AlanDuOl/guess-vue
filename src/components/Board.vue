08c3abbbb8eb2808ce5886ed9961550b18c91c3f
alanduarte90 <alanduarte90@gmail.com>
2019-05-28 23:31:37 -0300

    Initial commit

---
+++ src/components/Board.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="board-container">
+        <tile v-for="(item, index) in imgTiles" :key="index" :item="item">
+        </tile>
+    </div>
+</template>
+
+<script>
+import Tile from './Tile'
+export default {
+    name: 'board',
+    data: () => {
+        return {
+            numTiles: 8,
+            imgTiles: [1,1,1,1,1,1,1,1]
+        }
+    },
+    components: {
+        'tile': Tile
+    }
+}
+</script>
+
+<style>
+
+    #board-container {
+        display: flex;
+        width: 500px;
+        height: 500px;
+        border: 1px solid black;
+        margin: auto;
+    }
+
+</style>
---
+++ src/components/Tile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="board-tiles"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+    .board-tiles {
+        border: 1px solid red;
+        width: 50px;
+        height: 50px;
+    }
+
+</style>
